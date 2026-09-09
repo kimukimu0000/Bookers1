9f792a8750816f3906eebb1b8cfd072a58277ab3
Ubuntu <ubuntu@ip-172-31-30-38.ap-northeast-1.compute.internal>
2026-09-09 12:00:12 +0000

    [Add] ルーティング機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
-  get "homes/top"
+Rails.application.routes.draw development
+  root to: "homes#top"
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
