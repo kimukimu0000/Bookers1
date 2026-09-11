0e1c208511494bd6c5cd1857041b5e1e735eb89b
Ubuntu <ubuntu@ip-172-31-30-38.ap-northeast-1.compute.internal>
2026-09-11 08:28:15 +0000

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,43 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.save
+  @book = Book.new(book_params)
+  if @book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :index, status: :unprocessable_entity
   end
+end
+
+  def show
+  @book = Book.find(params[:id])
+end
+
+def edit
+  @book = Book.find(params[:id])
+end
+
+def update
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  @book = Book.find(params[:id])
+  @book.destroy
+  redirect_to books_path
+end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
+end
